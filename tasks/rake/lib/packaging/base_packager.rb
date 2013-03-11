26c9dab961eeb2e4021bd9ced9ee7b96e4bf89bc
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-11 17:11:31 +0100

    Framework for building shell agent rpms

---
+++ tasks/rake/lib/packaging/base_packager.rb
@@ -0,0 +1,80 @@
+gem 'fpm', '<=0.3.11'
+require 'fpm'
+require 'fpm/program'
+require 'pp'
+
+$:.unshift(File.join(File.dirname(__FILE__), '..'))
+require 'version_helper'
+
+class BasePackager
+
+  def initialize(package_type)
+    self.validate_environment
+
+    version_helper = VersionHelper.new
+
+    @basedirectory = ENV['WORKSPACE']
+    @semver_version = version_helper.semver_version
+    @release = "1"
+    @package_type = package_type
+
+    case package_type
+    when "rpm"
+      @first_delimiter, @second_delimiter, @architecture = "-", ".", "noarch"
+    when "deb"
+      @first_delimiter, @second_delimiter, @architecture = "_", "_", "all"
+    end
+  end
+
+  def validate_environment()
+    if ENV['WORKSPACE'].nil?
+      fail("Environment variable WORKSPACE has not been set.")
+    end
+    if ENV['BUILD_NUMBER'].nil?
+      ENV["BUILD_NUMBER"] = "0"
+    end
+    if ENV['GIT_COMMIT'].nil?
+      ENV['GIT_COMMIT'] = "54b0c58c7ce9f2a8b551351102ee0938"[0,10]
+    end
+  end
+
+  def build(plugin_name, plugin_dependencies={})
+    package_name = "cegeka-#{plugin_name}"
+    destination_file = "#{package_name}#{@first_delimiter}#{@semver_version}-#{@release}#{@second_delimiter}#{@architecture}.#{@package_type}"
+    destination_folder = "#{@basedirectory}/#{RESULTS}/dist"
+    description = "MCollective Plugin #{plugin_name} by Cegeka\n"
+
+    static_arguments = ["-t", @package_type, "-s", "dir", "-a", @architecture, "-m", "Cegeka <computing@cegeka.be>"]
+    exclude_arguments = ["-x", ".git", "-x", ".gitignore", "-x", "tasks", "-x", "Rakefile", "-x", "target", "-x", "README.md"]
+    var_arguments = ["-n", package_name, "-v", @semver_version, "--iteration", @release, "--description", description, "-C", "#{@basedirectory}/#{RESULTS}/input/#{plugin_name}", "usr"]
+    dependency_arguments = []
+    plugin_dependencies.each { |dependent_package,dependent_version|
+      dependency_arguments << "-d"
+      if dependent_version.empty?
+        dependency_arguments << "#{dependent_package}"
+      else
+        dependency_arguments << "#{dependent_package} = #{dependent_version}"
+      end
+    }
+    arguments = static_arguments + exclude_arguments + var_arguments + dependency_arguments
+
+    tmpdir = Dir.mktmpdir
+    Dir.chdir tmpdir
+    FileUtils.mkpath destination_folder
+    packagebuild = FPM::Program.new
+    ret = packagebuild.run(arguments)
+    FileUtils.mv("#{tmpdir}/#{destination_file}","#{destination_folder}/#{destination_file}")
+    FileUtils.remove_entry_secure(tmpdir)
+
+    puts "Saving #{package_name}.yaml file"
+    open("#{destination_folder}/#{package_name}.yaml", "w") { |file|
+      file.puts "package_name: #{package_name}"
+      file.puts "version: #{@semver_version}"
+      dependency_arguments.delete("-d")
+      file.puts "dependencies: #{dependency_arguments}"
+    }
+
+    return "Created #{destination_folder}/#{destination_file}"
+  end
+
+end
---
+++ tasks/rake/rpm.rake
@@ -0,0 +1,65 @@
+require File.expand_path('../../env', __FILE__)
+
+require 'yaml'
+
+$:.unshift(File.join(File.dirname(__FILE__), 'lib', 'packaging'))
+require 'rpm_packager'
+
+base_directory = File.join(File.dirname(__FILE__), '..', '..')
+
+desc "Prepare build environment"
+task :prepare_build_environment do
+  puts "Setting up the build environment"
+
+  FileUtils.mkpath("#{RESULTS}/dist")
+  FileUtils.mkpath("#{RESULTS}/input")
+end
+
+desc "Build mcollective common RPM"
+task :build_mcollective_common_rpm do
+  plugin_name = "mcollective-shell-common"
+
+  puts "Setting up build environment..."
+  FileUtils.mkpath("#{RESULTS}/input/#{plugin_name}/usr/libexec/mcollective/mcollective/agent")
+
+  FileUtils.copy("#{base_directory}/agent/shell.ddl", "#{RESULTS}/input/#{plugin_name}/usr/libexec/mcollective/mcollective/agent")
+
+  puts "Building #{plugin_name} RPM..."
+  rpm_packager = RpmPackager.new
+  output = rpm_packager.build(plugin_name)
+  puts output
+end
+
+desc "Build mcollective agent RPM"
+task :build_mcollective_agent_rpm do
+  plugin_name = "mcollective-shell-agent"
+
+  puts "Setting up build environment..."
+  FileUtils.mkpath("#{RESULTS}/input/#{plugin_name}/usr/libexec/mcollective/mcollective/agent")
+
+  FileUtils.copy("#{base_directory}/agent/shell.rb", "#{RESULTS}/input/#{plugin_name}/usr/libexec/mcollective/mcollective/agent")
+
+  version = YAML.load_file("#{RESULTS}/dist/cegeka-mcollective-shell-common.yaml")['version']
+
+  puts "Building #{plugin_name} RPM..."
+  rpm_packager = RpmPackager.new
+  output = rpm_packager.build(plugin_name, {"cegeka-mcollective-shell-common" => "#{version}", "mcollective-common" => "#{ENV['mcollective-common-version']}"})
+  puts output
+end
+
+desc "Build mcollective client RPM"
+task :build_mcollective_client_rpm do
+  plugin_name = "mcollective-shell-client"
+
+  puts "Setting up build environment..."
+  FileUtils.mkpath("#{RESULTS}/input/#{plugin_name}/usr/libexec/mcollective/mcollective/application")
+
+  FileUtils.copy("#{base_directory}/application/shell.rb", "#{RESULTS}/input/#{plugin_name}/usr/libexec/mcollective/mcollective/application")
+
+  version = YAML.load_file("#{RESULTS}/dist/cegeka-mcollective-shell-common.yaml")['version']
+
+  puts "Building #{plugin_name} RPM..."
+  rpm_packager = RpmPackager.new
+  output = rpm_packager.build(plugin_name, {"cegeka-mcollective-shell-common" => "#{version}", "mcollective-common" => "#{ENV['mcollective-common-version']}"})
+  puts output
+end
